ba058a1a4d6fa95bb3001b0cc7c1f20d1ab2f8f6
radiansz <lightwaveinthespace@gmail.com>
2017-03-31 13:23:19 +0300

    feat(interface): add support of end actions described in creators

--- src/index.js
+++ src/index.js
@@ -1,24 +1,34 @@
 import { takeEvery } from 'redux-saga';
-import { take, put, fork, call, race } from 'redux-saga/effects';
+import { take, put, fork, race } from 'redux-saga/effects';
 
 export const SUBMIT_FORM = '@@react-forml/SUBMIT_FORM';
 
-export function submitForm(values, { sourceCreater, successAction, failureAction, resolve, reject }) {
-  return { type: SUBMIT_FORM, payload: values, meta: { sourceCreater, successAction, failureAction, resolve, reject } };
+export function submitForm(values, { sourceCreator, successAction, failureAction, resolve, reject }) {
+  return { type: SUBMIT_FORM, payload: values, meta: { sourceCreator, successAction, failureAction, resolve, reject } };
 }
 
-export function bindSubmitFormToPromise(sourceCreater, successAction, failureAction, dispatch) {
+export function bindSubmitFormToPromise(...params) {
+  let [sourceCreator, successAction, failureAction, dispatch] = params;
+
+  if (typeof successAction === 'function') {
+    [sourceCreator, dispatch] = params;
+    successAction = undefined;
+    failureAction = undefined;
+  }
+
   return (...values) => new Promise((resolve, reject) => {
-    dispatch(submitForm(values, { sourceCreater, successAction, failureAction, resolve, reject }));
+    dispatch(submitForm(values, { sourceCreator, successAction, failureAction, resolve, reject }));
   });
 }
 
 function* dispatchSourceAction({ payload, meta }) {
-  yield put(meta.sourceCreater(...payload));
+  const sourceAction = meta.sourceCreator(...payload);
+
+  yield put(sourceAction);
 
   const { success, failure } = yield race({
-    success: take(meta.successAction),
-    failure: take(meta.failureAction),
+    success: take(meta.successAction || sourceAction.meta.successAction),
+    failure: take(meta.failureAction || sourceAction.meta.failureAction),
   });
 
   if (success) {
